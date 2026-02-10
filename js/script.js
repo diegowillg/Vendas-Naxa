bb5e82fbb2b6ea0b7a85fae2a86a4650d9d19a57
Diego <diegowilliam096@gmail.com>
2026-02-09 22:49:44 -0300

    Commit 1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function showSuccessMessage(event) {
+    event.preventDefault();
+    document.getElementById("successMessage").style.display = "block";
+        document.getElementById("formulario").reset();
+}
